aa3fc63df8bd566659dcc99e26850f711817525a
turgutmuslu <72387188+turgutmuslu@users.noreply.github.com>
2021-03-10 17:03:00 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -8,9 +8,9 @@ lines of 14-16: set a "for" loop on i as index, in line 15 i did the multiply wo
 even = [0, 2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd)  # here i merge the lists
-even.sort()  # here i sort the elements
+even.extend(odd)
+even.sort()
 
 for i in range(10):
-    even[i] *= 2  # this line does multiply work
-    print(even[i])  # and this prints the newest elements out
+    even[i] *= 2
+    print(even[i])
