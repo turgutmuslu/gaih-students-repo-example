fd9b048052bc11fd114a86f379e5d949e71d7d9f
turgutmuslu <72387188+turgutmuslu@users.noreply.github.com>
2021-03-10 17:02:41 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,16 @@
-#Explain your work
+"""
+lines of 8-9: defined the lists
+line of 11: merged the lists
+line 12: sorted the elements of new "even" list
+lines of 14-16: set a "for" loop on i as index, in line 15 i did the multiply work and in line 16 printed the newest elements out 
+"""
 
-#Question 1
-for x in range(a):
-	print(a)
+even = [0, 2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+even.extend(odd)  # here i merge the lists
+even.sort()  # here i sort the elements
+
+for i in range(10):
+    even[i] *= 2  # this line does multiply work
+    print(even[i])  # and this prints the newest elements out
