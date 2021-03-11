d0d871fd4a12624ba9a0d4aa4a7d0673cdbf7a4b
turgutmuslu <72387188+turgutmuslu@users.noreply.github.com>
2021-03-11 17:16:50 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,16 @@
-#Explain your work
+# i created cv dictionary, and then created 5 empty keys which symbolizes people. I filled them with unvalued attributes by a "for" loop. Then set another "for" loop to get inputs from user. And set the last "for" loop to print CV's.
 
-#Question 1
-for x in range(a):
-	print(a)
+cv = {"hasan":"", "nurbanu":"", "pembe":"", "kemal":"", "reyhan":""}
+
+for i in cv:
+    cv[i] = {"name-surname":"", "age":"", "job title":"", "foreign languages":""}
+
+for i in cv:
+    for j in cv[i]:
+        print(f"Please enter {j} of {i}")
+        cv[i][j] = str(input())
+
+for i in cv:
+    print(f"\n{i}'s CV:")
+    for j in cv[i]:
+        print(f" {j}: {cv[i][j]}")
